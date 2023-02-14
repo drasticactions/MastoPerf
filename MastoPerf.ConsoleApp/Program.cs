6e88d5735120c1938e2dbb353a94f0ff1d94045e
Timothy Miller <innerlogic4321@gmail.com>
2023-02-15 00:18:59 +0900

    Add console test

---
+++ MastoPerf.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mastonet.Entities;
+using MastoPerf;
+
+Console.WriteLine("Hello, Mastodon!");
+
+var test = new TestViewModel();
+
+test.Statuses.CollectionChanged += (sender, args) =>
+{
+    if (args.NewItems?.Count > 0)
+        Console.WriteLine(((Status)args.NewItems[0]).Content);
+};
+
+while(true)
+{
+}
