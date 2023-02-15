94995235f38095a854fe2146cb250d128303a40f
Timothy Miller <innerlogic4321@gmail.com>
2023-02-15 17:27:06 +0900

    Add WPF

---
+++ MastoPerf.WPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MastoPerf.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.TestViewModel = new TestViewModel();
+            this.MainGrid.DataContext = this.TestViewModel;
+        }
+
+        public TestViewModel TestViewModel { get; set; } = new TestViewModel();
+
+        private void Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            this.TestViewModel.StartStreaming();
+        }
+
+        private void Button_Clicked_1(object sender, RoutedEventArgs e)
+        {
+            this.TestViewModel.EndStreaming();
+        }
+    }
+}
