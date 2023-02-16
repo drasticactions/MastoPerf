72d80e72a7109e28e5a2f761401d327b2fa9fad9
Timothy Miller <innerlogic4321@gmail.com>
2023-02-16 21:05:31 +0900

    Add XF

---
+++ MastoPerf.XamarinForms/MastoPerf.XamarinForms/MastoPerf.XamarinForms/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MastoPerf.XamarinForms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this.TestViewModel;
+        }
+
+        public TestViewModel TestViewModel { get; set; } = new TestViewModel();
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            this.TestViewModel.StartStreaming();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            this.TestViewModel.EndStreaming();
+        }
+    }
+}
